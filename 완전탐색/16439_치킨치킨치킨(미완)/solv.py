bb26add60f4f558be2d1ae07d34f048570340fc1
sojung13 <bestsojung13@gmail.com>
2023-04-04 23:38:35 +0900

    2023.04.04

--- 완전탐색/16439_치킨치킨치킨(미완)/solv.py
+++ 완전탐색/16439_치킨치킨치킨(미완)/solv.py
@@ -1,4 +1,17 @@
 import sys
 sys.stdin = open('input.txt','r')
+from itertools import combinations
 
 n,m = map(int,input().split())
+arr = []
+for _ in range(n):
+    arr.append(list(map(int,input().split())))
+
+maxSum = 0
+for a,b,c in combinations(range(m),3):
+    print(a,b,c)
+    tmpSum = 0
+    for i in range(n):
+        tmpSum += max(arr[i][a], arr[i][b], arr[i][c])
+    maxSum = max(maxSum, tmpSum)
+print(maxSum)
